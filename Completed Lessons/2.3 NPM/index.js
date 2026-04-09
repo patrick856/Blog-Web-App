dd5cebbffb6d12b44b9e1352e257f045babe55fd
patrick856 <patricknotb2@gmail.com>
2026-04-09 16:46:19 +0200

    Revert "functionality + simple css"
    This reverts commit 3c5a5341f9fd91d8ae4f76db0ca2efead9744f29.

--- Completed Lessons/2.3 NPM/index.js
+++
@@ -1,9 +0,0 @@
-import generateName from "sillyname";
-import {randomSuperhero} from 'superheroes';
-
-var sillyName = generateName();
-var superhero = randomSuperhero();
-
-console.log(sillyName);
-console.log(superhero);
-
--- Completed Lessons/Dicee+Challenge+-+Starting+Files/script.js
+++
@@ -1,7 +0,0 @@
-var player1 = Math.ceil(Math.random()*6);
-var player2 = Math.ceil(Math.random()*6);
-
-document.querySelector(".img1").setAttribute("src", "images/dice" + player1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + player2 + ".png");
-
-document.querySelector("h1").innerHTML = (player1 > player2) ? "Player 1 Wins!" : (player1 < player2) ? "Player 2 Wins!" : "Draw!";
